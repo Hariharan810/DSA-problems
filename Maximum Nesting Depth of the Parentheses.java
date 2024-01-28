54ac114a1ae985fd8547abd78aecef727ed1aca0
Hariharan V <118278198+Hariharan810@users.noreply.github.com>
2024-01-28 12:52:26 +0530

    Create Maximum Nesting Depth of the Parentheses.java

---
+++ Maximum Nesting Depth of the Parentheses.java
@@ -0,0 +1,23 @@
+//LeetCode
+//Problem Link : https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/description/
+
+class Solution {
+    public int maxDepth(String s) {
+        
+        int ans=0;
+        int temp=0;
+
+        for(char i : s.toCharArray()){
+            if(i=='('){
+                temp++;
+            }
+            if(i==')'){
+                temp--;
+            }
+            if(temp>ans){
+                ans=temp;
+            }
+        }
+        return ans;
+    }
+}
