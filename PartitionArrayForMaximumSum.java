84d9f6fd79deeb7a5e27f1cd23888c442534f427
Hari <hariharan19082003@gmail.com>
2024-02-03 08:28:23 +0530

    Create PartitionArrayForMaximumSum

---
+++ PartitionArrayForMaximumSum.java
@@ -0,0 +1,37 @@
+
+// LeetCode
+// Problem Link : https://leetcode.com/problems/partition-array-for-maximum-sum/?envType=daily-question&envId=2024-02-03
+
+import java.util.Arrays;
+
+public class PartitionArrayForMaximumSum {
+    public int maxSumAfterPartitioning(int[] arr, int k) {
+
+        int n = arr.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp,-1);
+        return findMaxSum(0,arr,k,dp);
+    }
+    public static int findMaxSum(int ind,int[] arr,int k,int[] dp){
+
+        int n=arr.length;
+        if(ind==n)return 0;
+
+        if(dp[ind]!=-1)return dp[ind];
+
+        int len=0;
+        int max=Integer.MIN_VALUE;
+        int maxSum=Integer.MIN_VALUE;
+
+        for(int i=ind;i<Math.min(ind+k,n);i++){
+
+            len++;
+            max=Math.max(max,arr[i]);
+
+            int sum = len*max + findMaxSum(i+1,arr,k,dp);
+
+            maxSum=Math.max(maxSum,sum);
+        }
+        return dp[ind]=maxSum;
+    }
+}
