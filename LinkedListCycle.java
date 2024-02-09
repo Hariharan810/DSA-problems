c9d15b8613fe35f192b0358dd1cf9558b7f507df
Hari <hariharan19082003@gmail.com>
2024-02-09 20:21:26 +0530

    updated Linked List Problems

---
+++ LinkedListCycle.java
@@ -0,0 +1,34 @@
+
+// LeetCOde
+// Problem Link : https://leetcode.com/problems/linked-list-cycle/
+
+
+public class LinkedListCycle {
+
+    public boolean hasCycle(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast!=null && fast.next!=null){
+
+            fast=fast.next.next;
+            slow=slow.next;
+
+            if(fast == slow){
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    class ListNode {
+     int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+    }
+}
