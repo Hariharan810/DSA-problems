2066e4243fb40f5eec8550a679c0286930599af9
Hari <hariharan19082003@gmail.com>
2024-02-22 19:19:57 +0530

    Create FindTheTownJudge

---
+++ FindTheTownJudge.java
@@ -0,0 +1,21 @@
+
+// LeetCode
+// Problem Link : https://leetcode.com/problems/find-the-town-judge/description/?envType=daily-question&envId=2024-02-22
+
+public class FindTheTownJudge {
+
+    public int findJudge(int n, int[][] trust) {
+
+        int[] result = new int[n+1];
+
+        for(int[] relation : trust){
+            result[relation[0]]--;
+            result[relation[1]]++;
+        }
+
+        for(int i=1;i<=n;i++){
+            if(result[i] == n-1)return i;
+        }
+        return -1;
+    }
+}
