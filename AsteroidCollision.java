6e2d8431b1c8e6bc219702225ff19073f103dfac
Hari <hariharan19082003@gmail.com>
2024-02-28 19:37:07 +0530

    Create TrappingRainWater

---
+++ AsteroidCollision.java
@@ -0,0 +1,41 @@
+
+
+// LeetCode
+// Problem Link : https://leetcode.com/problems/asteroid-collision/
+
+
+import java.util.Stack;
+
+public class AsteroidCollision {
+    public int[] asteroidCollision(int[] asteroids) {
+        int n = asteroids.length;
+
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i=0;i<n;i++){
+
+            if(asteroids[i]>0 || stack.isEmpty()){
+                stack.push(asteroids[i]);
+            }
+            else{
+                while(!stack.isEmpty() && stack.peek()>0 && stack.peek()<Math.abs(asteroids[i])){
+                    stack.pop();
+                }
+                if(!stack.isEmpty() && stack.peek()==Math.abs(asteroids[i])){
+                    stack.pop();
+                }
+                else{
+                    if(stack.isEmpty() || stack.peek()<0){
+                        stack.push(asteroids[i]);
+                    }
+                }
+            }
+        }
+
+        int[] result = new int[stack.size()];
+        for(int i=result.length-1;i>=0;i--){
+            result[i]=stack.pop();
+        }
+        return result;
+    }
+}
---
+++ MinimumAddToMakeParenthesesValid.java
@@ -0,0 +1,31 @@
+
+// LeetCode
+// Problem Link : https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/
+
+
+
+import java.util.Stack;
+
+public class MinimumAddToMakeParenthesesValid {
+
+    public int minAddToMakeValid(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for(char ch : s.toCharArray()){
+
+            if(ch == '('){
+                stack.push(ch);
+            }
+            else{
+                if(stack.size()>0 && stack.peek()=='('){
+                    stack.push('(');
+                    stack.pop();
+                }else{
+                    stack.push(ch);
+                }
+            }
+        }
+        return stack.size();
+    }
+}
