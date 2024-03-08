895318f75086117955e628c34d5b34ce8c174e57
Hari <hariharan19082003@gmail.com>
2024-03-08 20:22:11 +0530

    Create CountElementsWithMaximumFrequency

---
+++ CountElementsWithMaximumFrequency.java
@@ -0,0 +1,26 @@
+
+//LeetCode: https://leetcode.com/problems/count-number-of-homogenous-substrings/
+
+
+import java.util.HashMap;
+
+public class CountElementsWithMaximumFrequency {
+    public int maxFrequencyElements(int[] nums) {
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        for (int num : nums) {
+            mp.put(num, mp.getOrDefault(num, 0) + 1);
+        }
+
+        int count = 0;
+        int maxFreq = Integer.MIN_VALUE;
+        for (int freq : mp.values()) {
+            maxFreq = Math.max(maxFreq, freq);
+        }
+
+        for (int freq : mp.values()) {
+            if (freq == maxFreq)
+                count += maxFreq;
+        }
+        return count;
+    }
+}
