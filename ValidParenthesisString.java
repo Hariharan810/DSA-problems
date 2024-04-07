bcddc5f8fc7c3bf571088d3c947edd0377099856
Hariharan <hariharan19082003@gmail.com>
2024-04-07 13:05:47 +0530

    create ValidParenthesisString

---
+++ ValidParenthesisString.java
@@ -0,0 +1,25 @@
+
+//LeetCode: https://leetcode.com/problems/valid-parenthesis-string/
+
+public class ValidParenthesisString {
+    public boolean checkValidString(String s) {
+        int leftMin = 0, leftMax = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                leftMin++;
+                leftMax++;
+            } else if (c == ')') {
+                leftMin--;
+                leftMax--;
+            } else {
+                leftMin--;
+                leftMax++;
+            }
+            if (leftMax < 0) return false;
+            if (leftMin < 0) leftMin = 0;
+        }
+
+        return leftMin == 0;
+    }
+}
