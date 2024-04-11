d74b0c537295bcbab1caf5030cdd8376cda0eb11
Hariharan <hariharan19082003@gmail.com>
2024-04-11 08:17:22 +0530

    Create MinimizeTheSumOfProduct

---
+++ MinimizeTheSumOfProduct.java
@@ -0,0 +1,22 @@
+
+// LeetCode: https://leetcode.com/problems/minimize-the-sum-of-product/
+
+
+import java.util.Arrays;
+
+public class MinimizeTheSumOfProduct {
+
+    public long minValue(long a[], long b[], long n) {
+
+        Arrays.sort(a);
+
+        Arrays.sort(b);
+
+        long result = 0;
+
+        for(int i = 0; i < a.length; i++) {
+            result += a[i] * b[(int)n-i-1];
+        }
+        return result;
+    }
+}
