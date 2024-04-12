c66e1696fab3c63ef2f073e78cd1f7fd7bfb835d
Hariharan <hariharan19082003@gmail.com>
2024-04-12 06:28:36 +0530

    updated

--- Demo.java
+++ Demo.java
@@ -3,24 +3,18 @@ import java.util.Stack;
 
 public class Demo {
     public static void main(String[] args) {
-        int[] a={4, 7, 8, 2, 3, 1};
+        int[] nums = {12,6,1,2,7};
 
-        int n = a.length;
-
-        Stack<Integer> stack = new Stack<>();
-        for (int i = n - 1; i >= 0; i--) {
-            int cur = a[i];
-            while (!stack.isEmpty() && stack.peek() >= cur) {
-                stack.pop();
-            }
-            if (!stack.isEmpty()) {
-                a[i]=stack.peek();
-            } else {
-                a[i]=-1;
-            }
-
-            stack.push(cur);
+        long score = 0;
+        int[] maxLeft = new int[nums.length], maxRight = new int[nums.length];;
+        maxLeft[0] = nums[0];
+        maxRight[nums.length-1] = nums[nums.length-1];
+        for(int i=1; i<nums.length; i++) maxLeft[i] = Math.max(maxLeft[i-1], nums[i]);
+        for(int i=nums.length-2; i>=0; i--)   maxRight[i] = Math.max(maxRight[i+1], nums[i]);
+        for (int i=1; i<nums.length-1; i++) {
+            long currScore = (long)(maxLeft[i-1]-nums[i])*maxRight[i+1];
+            score = Math.max(score, currScore);
         }
-        System.out.println(Arrays.toString(a));
+        System.out.println(score);
     }
 }
