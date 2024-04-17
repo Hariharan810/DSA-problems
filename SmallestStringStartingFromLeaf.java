2b71661f2033cfd57b6b8221a263f9dc66ef6d42
Hariharan <hariharan19082003@gmail.com>
2024-04-17 06:54:00 +0530

    Create SmallestStringStartingFromLeaf

---
+++ SmallestStringStartingFromLeaf.java
@@ -0,0 +1,50 @@
+
+// LeetCode : https://leetcode.com/problems/smallest-string-starting-from-leaf/
+
+
+public class SmallestStringStartingFromLeaf {
+    class Solution {
+        String smallestString = null;
+
+        public String smallestFromLeaf(TreeNode root) {
+            dfs(root, new StringBuilder());
+            return smallestString;
+        }
+
+        private void dfs(TreeNode node, StringBuilder currentString) {
+            if (node == null) {
+                return;
+            }
+
+            currentString.insert(0, (char) ('a' + node.val));
+
+            if (node.left == null && node.right == null) {
+                updateSmallestString(currentString.toString());
+            } else {
+                dfs(node.left, currentString);
+                dfs(node.right, currentString);
+            }
+
+            currentString.deleteCharAt(0);
+        }
+
+        private void updateSmallestString(String currentString) {
+            if(smallestString == null||currentString.compareTo(smallestString)< 0) {
+                smallestString = currentString;
+            }
+        }
+    }
+
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+}
