3d450fdc90e673c92ce223d4ce507a035bd7c6e9
Hariharan <hariharan19082003@gmail.com>
2024-04-22 18:59:07 +0530

    Create ReverseALinkedList

---
+++ ReverseALinkedList.java
@@ -0,0 +1,30 @@
+
+// GFG : https://practice.geeksforgeeks.org/problems/reverse-a-linked-list/1
+
+public class ReverseALinkedList {
+    Node reverseList(Node head)
+    {
+        // code here
+
+        Node prev = null;
+        Node temp = head;
+
+        while(temp != null){
+
+            Node front = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = front;
+        }
+
+        return prev;
+    }
+    class Node {
+        int data;
+        Node next;
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+}
