00930dc81831c058eab03e241ff71cd66edee05d
Hariharan <hariharan19082003@gmail.com>
2024-04-23 05:41:37 +0530

    Create MinimumHeightTrees

---
+++ MinimumHeightTrees.java
@@ -0,0 +1,65 @@
+
+// LeetCode : https://leetcode.com/problems/minimum-height-trees/
+
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MinimumHeightTrees {
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+        if(edges.length == 0 || n == 1){
+            List<Integer> res = Arrays.asList(0);
+            return res;
+        }
+        ArrayList<Integer>[] adj = new ArrayList[n];
+        int[] indegree = new int[n];
+
+        for(int i=0; i<n; i++){
+            adj[i] = new ArrayList<>();
+        }
+
+        for(int[] e: edges){
+            int v = e[0];
+            int u = e[1];
+
+            adj[v].add(u);
+            adj[u].add(v);
+            indegree[u]++;
+            indegree[v]++;
+        }
+
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
+
+        for(int i=0; i<n; ++i){
+            if(indegree[i] == 1){
+                queue.add(i);
+            }
+        }
+
+        while(n > 2){
+            int size = queue.size();
+            n -= size;
+
+            while(size-- > 0){
+                int rem = queue.remove();
+
+                for(int i: adj[rem]){
+                    indegree[i]--;
+                    if(indegree[i] == 1){
+                        queue.add(i);
+                    }
+                }
+            }
+        }
+
+        ArrayList<Integer> res = new ArrayList<>();
+        while(queue.size() > 0){
+            res.add(queue.pop());
+        }
+
+
+        return res;
+    }
+}
