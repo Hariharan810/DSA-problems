06206b335bb8cc11995bed0b6662dac7a656a29d
Hariharan <hariharan19082003@gmail.com>
2024-05-07 20:10:01 +0530

    Create DoubleANumberRepresentedAsALinkedList

--- DoubleANumberRepresentedAsALinkedList.java
+++ DoubleANumberRepresentedAsALinkedList.java
@@ -39,6 +39,7 @@ public class DoubleANumberRepresentedAsALinkedList {
         ListNode(int val) {
             this.val = val;
         }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 
     }
 }
