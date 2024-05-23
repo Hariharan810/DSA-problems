6258af3260af0bfba101c1f7f6bc03a88eb974d0
Hariharan <hariharan19082003@gmail.com>
2024-05-23 10:01:11 +0530

    Create TheNumberOfBeautifulSubsets

---
+++ TheNumberOfBeautifulSubsets.java
@@ -0,0 +1,34 @@
+
+// LeetCode : https://leetcode.com/problems/the-number-of-beautiful-subsets/
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TheNumberOfBeautifulSubsets {
+    public int beautifulSubsets(int[] nums, int k) {
+
+        Map<Integer, Integer> m = new HashMap<>();
+
+        for (int num : nums) m.put(num, m.getOrDefault(num, 0) + 1);
+
+        int res = 1, prev = 0, prevPrev = 0;
+
+        for (Map.Entry<Integer, Integer> e : m.entrySet()) {
+            int cur = e.getKey();
+
+            if (m.containsKey(cur - k)) continue;
+
+            prev = 0;
+
+            while (m.containsKey(cur)) {
+                prevPrev = prev;
+                prev = ((1 << m.get(cur)) - 1) * res;
+                res += prevPrev;
+                cur += k;
+            }
+            res += prev;
+        }
+        return res - 1;
+    }
+}
