d2165887029998f03bff81b9a51144a26902270a
Hariharan <hariharan19082003@gmail.com>
2024-06-09 20:59:44 +0530

    Create SubarraySumsDivisibleByK

---
+++ SubarraySumsDivisibleByK.java
@@ -0,0 +1,30 @@
+
+// LeetCode : https://leetcode.com/problems/subarray-sums-divisible-by-k/
+
+
+import java.util.HashMap;
+
+public class SubarraySumsDivisibleByK {
+    public int subarraysDivByK(int[] nums, int k) {
+
+        HashMap<Integer,Integer> map=new HashMap<>();
+        map.put(0,1);
+        int prefix_sum=0;
+        int count=0;
+        for(int i=0; i<nums.length; i++){
+            prefix_sum+=nums[i];
+            int rem=prefix_sum % k;
+            if(rem<0){
+                rem=(rem+k);
+            }
+            if(map.containsKey(rem)){
+                count+=map.get(rem);
+                map.put(rem,map.get(rem)+1);
+            }else{
+                map.put(rem,1);
+            }
+
+        }
+        return count;
+    }
+}
