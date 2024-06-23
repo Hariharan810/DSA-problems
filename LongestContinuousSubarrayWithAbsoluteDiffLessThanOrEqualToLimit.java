fcdd4b4f7ab9659d6fc6abe0bba7af2ceafd4658
Hariharan <hariharan19082003@gmail.com>
2024-06-23 20:06:48 +0530

    Create LongestContinuousSubarrayWithAbsoluteDiffLessThanOrEqualToLimit

---
+++ LongestContinuousSubarrayWithAbsoluteDiffLessThanOrEqualToLimit.java
@@ -0,0 +1,43 @@
+
+// LeetCode : https://leetcode.com/problems/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/
+
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class LongestContinuousSubarrayWithAbsoluteDiffLessThanOrEqualToLimit {
+    public int longestSubarray(int[] nums, int limit) {
+        Deque<Integer> decQ = new LinkedList<>();
+        Deque<Integer> incQ = new LinkedList<>();
+        int ans = 0;
+        int left = 0;
+
+        for (int right = 0; right < nums.length; ++right) {
+            int num = nums[right];
+
+            while (!decQ.isEmpty() && num > decQ.peekLast()) {
+                decQ.pollLast();
+            }
+            decQ.addLast(num);
+
+            while (!incQ.isEmpty() && num < incQ.peekLast()) {
+                incQ.pollLast();
+            }
+            incQ.addLast(num);
+
+            while (decQ.peekFirst() - incQ.peekFirst() > limit) {
+                if (decQ.peekFirst() == nums[left]) {
+                    decQ.pollFirst();
+                }
+                if (incQ.peekFirst() == nums[left]) {
+                    incQ.pollFirst();
+                }
+                ++left;
+            }
+
+            ans = Math.max(ans, right - left + 1);
+        }
+
+        return ans;
+    }
+}
