075990327b2670892dd277b2ca4a6f8b3f643f74
Hariharan <hariharan19082003@gmail.com>
2024-06-24 18:40:29 +0530

    Create MinimumNumberOfKConsecutiveBitFlips

---
+++ MinimumNumberOfKConsecutiveBitFlips.java
@@ -0,0 +1,24 @@
+
+// LeetCode : https://leetcode.com/problems/minimum-number-of-k-consecutive-bit-flips/
+
+
+public class MinimumNumberOfKConsecutiveBitFlips {
+    public int minKBitFlips(int[] nums, int k) {
+        int ans = 0, flips = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if ((nums[i] + flips) % 2 == 0) {
+                if (i > nums.length - k) {
+                    return -1;
+                } else {
+                    ans++;
+                    flips++;
+                    nums[i] = -1;
+                }
+            }
+            if (i + 1 >= k) {
+                flips -= (nums[i - k + 1] < 0 ? 1 : 0);
+            }
+        }
+        return ans;
+    }
+}
