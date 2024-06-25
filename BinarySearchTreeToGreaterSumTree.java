f6dde19c420f07b09d8b8d1921f67b86cec3d3e0
Hariharan <hariharan19082003@gmail.com>
2024-06-25 22:01:48 +0530

    Create BinarySearchTreeToGreaterSumTree

---
+++ BinarySearchTreeToGreaterSumTree.java
@@ -0,0 +1,23 @@
+public class BinarySearchTreeToGreaterSumTree {
+    int val = 0;
+    public TreeNode bstToGst(TreeNode root) {
+        if (root.right != null) bstToGst(root.right);
+        val = root.val = val + root.val;
+        if (root.left != null) bstToGst(root.left);
+        return root;
+    }
+
+
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+    }
+}
