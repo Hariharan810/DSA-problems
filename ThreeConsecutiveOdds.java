196b4bc9e8b608be18927e100f82015f356ba6e6
Hariharan <hariharan19082003@gmail.com>
2024-07-01 07:59:32 +0530

    Create ThreeConsecutiveOdds

---
+++ ThreeConsecutiveOdds.java
@@ -0,0 +1,25 @@
+
+// LeetCode : https://leetcode.com/problems/three-consecutive-odds/
+
+
+public class ThreeConsecutiveOdds {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int count  = 0;
+
+        for(int i=0;i<arr.length;i++){
+
+            if(arr[i]%2 == 0){
+                count = 0;
+            }
+
+            else{
+                count++;
+            }
+
+            if(count == 3){
+                return true;
+            }
+        }
+        return false;
+    }
+}
