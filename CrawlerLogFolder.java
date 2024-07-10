36f6204dca27e20a2ce2e594cefa7b62487ecf70
Hariharan <hariharan19082003@gmail.com>
2024-07-10 07:10:40 +0530

    Create CrawlerLogFolder

---
+++ CrawlerLogFolder.java
@@ -0,0 +1,23 @@
+
+// LeetCode : https://leetcode.com/problems/crawler-log-folder/
+
+
+public class CrawlerLogFolder {
+    public int minOperations(String[] logs) {
+
+        int result = 0;
+
+        for(String log:logs){
+            if(log.equals("../")){
+                if(result > 0){
+                    result--;
+                }
+            }
+            else if(!log.equals("./")){
+                result++;
+            }
+        }
+        return result;
+
+    }
+}
