cf7e6b6cf16279deb28efce9852893512581b351
Hariharan <hariharan19082003@gmail.com>
2024-07-12 08:08:21 +0530

    Create SameTree

---
+++ SameTree.java
@@ -0,0 +1,30 @@
+public class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+
+        if(p == null || q == null){
+            return p == q;
+        }
+
+        return (p.val == q.val) && isSameTree(p.left,q.left) &&
+                isSameTree(p.right,q.right);
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
