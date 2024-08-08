f4cf9a1ad3ead11d96cc79133afdeb25f9c749bc
Hariharan <hariharan19082003@gmail.com>
2024-08-08 18:25:48 +0530

    Create SpiralMatrixIII

---
+++ SpiralMatrixIII.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpiralMatrixIII {
+    public int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
+        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int[][] result = new int[rows * cols][2];
+        int steps = 0, d = 0, len = 0;
+
+        result[0] = new int[]{rStart, cStart};
+        int count = 1;
+
+        while (count < rows * cols) {
+            if (d == 0 || d == 2) steps++;
+            for (int i = 0; i < steps; i++) {
+                rStart += directions[d][0];
+                cStart += directions[d][1];
+
+                if (rStart >= 0 && rStart < rows && cStart >= 0 && cStart < cols) {
+                    result[count++] = new int[]{rStart, cStart};
+                }
+
+                if (count == rows * cols) return result;
+            }
+
+            d = (d + 1) % 4;
+        }
+
+        return result;
+    }
+}
