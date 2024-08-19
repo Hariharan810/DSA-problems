c09624b038178292906165b9bd8921b5d311d6ab
Hariharan <hariharan19082003@gmail.com>
2024-08-19 09:55:54 +0530

    Create TwoKeysKeyboard

---
+++ TwoKeysKeyboard.java
@@ -0,0 +1,23 @@
+
+// LeetCode : https://leetcode.com/problems/2-keys-keyboard/
+
+
+public class TwoKeysKeyboard {
+    public int minSteps(int n) {
+
+        if(n == 1) return 0;
+
+        int steps = 0;
+        int factor = 2;
+
+        while(n > 1){
+            while(n % factor == 0){
+                steps += factor;
+                n /= factor;
+            }
+            factor++;
+        }
+
+        return steps;
+    }
+}
