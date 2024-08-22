331c5a3e6a3a71c7d69e6a1bf614cb9fa020349e
Hariharan <hariharan19082003@gmail.com>
2024-08-22 15:04:40 +0530

    Create NumberComplement

---
+++ NumberComplement.java
@@ -0,0 +1,19 @@
+
+// LeetCode : https://leetcode.com/problems/number-complement/
+
+
+public class NumberComplement {
+    public int findComplement(int num) {
+        String str = Integer.toBinaryString(num);
+        String res = "";
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i) == '0'){
+                res += '1';
+            }
+            else{
+                res += '0';
+            }
+        }
+        return Integer.parseInt(res,2);
+    }
+}
